4fa90acbe58cfec1bf6860aee39663e592fabe8f
Lachezar <lachkoy94@gmail.com>
2016-03-25 00:02:02 +0200

    minor changes

---
+++ src/main/java/com/model/Payment.java
@@ -0,0 +1,13 @@
+package com.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Payment {
+	
+	
+	private double amount;
+	private LocalDate date;
+	private Period period;
+	
+}
---
+++ src/main/java/com/model/User.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import java.util.List;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private double balance;
+	List<Payment> payments;
+}
