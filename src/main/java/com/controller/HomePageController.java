942ccd5830bd0d5c55c8fb7970b2d56a5fa27b47
Lachezar <lachkoy94@gmail.com>
2016-03-25 22:52:16 +0200

    minor changes in folders

---
+++ src/main/java/com/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@RequestMapping(value = "/index")
+@Controller
+public class HomePageController {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	String startUp() {
+		
+		return "index";
+	}
+}
