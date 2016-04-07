4741dd45930ee451ed4b7a02a310be7dc800ccaf
dsspasov <dsspasov94@gmail.com>
2016-04-07 12:11:20 +0300

    Simulator for planning savings added

---
+++ src/main/java/com/controller/SimulatorController.java
@@ -0,0 +1,90 @@
+package com.controller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model.Budget;
+import com.model.User;
+import com.model.db.DBBudgetDAO;
+
+@Controller
+public class SimulatorController {
+
+	@RequestMapping(value="/getBudget", method=RequestMethod.GET)
+	String budget() {	
+		return "simulator";
+	}
+	
+	
+	@RequestMapping(value="/getBudget", method=RequestMethod.POST)
+	String getBudget(@RequestParam(value="date") String d, HttpSession session, Model model) {
+		LocalDate date = LocalDate.of(LocalDate.now().getYear(), LocalDate.now().getMonthValue(), 1);
+		if(d.trim().length()!=0){
+			try{
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+			date = LocalDate.parse(d, formatter);
+			}catch(DateTimeParseException e){
+				model.addAttribute("error","invalid date");
+				return "simulator";
+			}
+		}
+		User user = (User)session.getAttribute("logedUser");
+		
+		Budget budget = DBBudgetDAO.getInstance().getBudget(user.getId(), date);
+		
+		System.out.println("Budget: " + budget);
+		
+		if(budget!=null){
+			session.setAttribute("budget", budget);
+			model.addAttribute("showBudget", budget);
+		}else{
+			session.removeAttribute("budget");
+			model.addAttribute("error","No such budget");
+		}
+		return "simulator";
+	}
+
+	@RequestMapping(value="/calculate", method=RequestMethod.POST)
+	String calculate(@RequestParam("period") double period, HttpServletRequest request, HttpSession session, Model model){
+		Budget budget = (Budget)(session.getAttribute("budget"));
+		//Budget budget = (Budget)(model.asMap().get("budget"));
+		
+		String[] incomesID = request.getParameterValues("income");
+		String[] expensesID = request.getParameterValues("expense");
+		
+		
+		double balance = 0;
+		if(incomesID!=null){
+			for(String id:incomesID){
+				int intId = Integer.valueOf(id);
+				balance += budget.getPayments().get("INCOME").get(intId).getAmount();
+			}
+		}
+		
+		//balance = balance * budget.getPercentageOfIncome();
+		
+		if(expensesID!=null){
+			for(String id:expensesID){
+				int intId = Integer.valueOf(id);
+				balance -= budget.getPayments().get("EXPENSE").get(intId).getAmount();
+			}
+		}	
+		
+		balance = balance*period;
+		model.addAttribute("result", balance);
+		model.addAttribute("showBudget",budget);
+		return "simulator";
+	}
+	
+	
+}
